8bc93d1c2dd5595943044b45835d974faada1edd
kyonmm <kyon.mm@gmail.com>
2018-10-01 18:51:30 +0900

    最低限のコンポーネントにした

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Thread
+      :threadStart="'hoge'"
+      :comments="['piyo', 'fuga']"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import Thread from './components/Thread.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Thread
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  comments: string[] = ["piyo", "foo"];
+}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Thread.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="thread">
+      <article class="media">
+      <figure class="media-left icon">
+          <p class="image is-64x64">
+              <img class="user-image" src="favicon.ico"/>
+          </p>
+      </figure>
+      <div class="media-content">
+          <div class="content">
+      <span class="screen-name"><strong>sample</strong></span>
+      <span class="time-lapsed"><small>3h</small></span>
+      <span class="reactions"/>
+      <span class="description thread-start">
+          {{threadStart}}
+    </span>
+      <ol>
+        <li v-for="comment in comments">
+            <div class="comment">
+                {{comment}}
+            </div>
+        </li>
+      </ol>
+      </div>
+      </div>
+      </article>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Thread extends Vue {
+  @Prop() private threadStart!: string;
+  @Prop() private comments!: string[];
+}
+</script>
+
+<style scoped lang="scss">
+</style>
